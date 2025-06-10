86076a1e952874809ce60d17f3856a09be89b863
Alaa Elsisy <elsisyalaa0@gmail.com>
2025-06-10 16:12:50 +0300

    deployment T1

--- BeBeauty/Program.cs
+++ BeBeauty/Program.cs
@@ -85,12 +85,12 @@ namespace BeBeauty
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                   
                 app.MapOpenApi();
                
-            }
+            //}
             app.UseRouting();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
